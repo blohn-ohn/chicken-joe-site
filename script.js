3d9daf5a90114172bf6abc4eb9e6570c767d752b
blohn-ohn <blohnohn@gmail.com>
2025-05-22 23:02:15 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    alert("Welcome to Peace! by Chicken Joe🌊");
+
+    document.querySelector("img").addEventListener("click", function() {
+        alert("'Stay chill, dude!'");
+    });
+});
